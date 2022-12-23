79c3a54da96adae18674dfd7edaf81a3b5c8d36c
ulises ros <ulisesros70@gmail.com>
2022-12-23 14:30:48 -0300

    colores y tipografia

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div
+    style={{
+      textAlign: "center"
+    }}
+    >
+      <h1>Proyecto Final React</h1>
+    </div>
+  )
+}
+
+export default App;
