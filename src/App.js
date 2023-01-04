3d55e35a27968f325e7031f343b54f7ca2ec0537
stefania gentile <stefania.gentile89@icloud.com>
2023-01-04 15:08:07 -0300

    add home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Box } from "@chakra-ui/react";
+import HomeSlider from "./components/Home/HomeSlider";
 import SeccionSmartphone from "./components/seccion-smartphone/SeccionSmartphone";
 import TarjetaEscritorio from "./components/seccion-smartphone/TarjetaEscrtorio";
 import TarjetaMobile from "./components/seccion-smartphone/TarjetaMobile";
-
 
 const App = () => {
   return (
     <Box>
       <SeccionSmartphone/>
+      <HomeSlider />
     </Box>
   )
 }
---
+++ src/components/Home/BotonPediLaTuya.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const BotonPediLaTuya = () => {
+  return (
+    <button style={{
+        background: 'var(--backg-color)',
+        color: 'var(--third-color)',
+        fontFamily: 'var(--first-font)',
+        fontSize: '1.3rem',
+        borderRadius: '10px',
+        width: '70%',
+        heigth: '2rem',
+    }}>
+        <b>Pedí la tuya! que estás esperando?</b>
+    </button>
+  )
+}
+
+export default BotonPediLaTuya
