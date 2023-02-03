7718643dc3e91f59ac8f1b31927c4711d802d49d
stefania gentile <stefania.gentile89@icloud.com>
2023-02-03 17:29:44 -0300

    modal en boton pineapple card

--- src/components/Home/BotonPediLaTuya.js
+++ src/components/Home/BotonPediLaTuya.js
@@ -4,15 +4,15 @@ import Swal from 'sweetalert2'
 const BotonPediLaTuya = () => {
   const pediLaTuya = () => {
     Swal.fire({
-        width: "500px",
-        title: "Dejanos tus datos y te enviaremos tu Pineapple Card",
-        html: `<input type="text" id="nombre" class="swal2-input" placeholder="Nombre completo">
-        <input type="text" id="dni" class="swal2-input" placeholder="DNI sin puntos ni espacios">
-  <input type="text" id="telefono" class="swal2-input" placeholder="+54 11 0000 0000">
-  <input type="text" id="mail" class="swal2-input" placeholder="tuemail@ejemplo.com">
-  <input type="text" id="direccion" class="swal2-input" placeholder="Direccion">`,
-  confirmButtonText: 'Enviar',
-    })
+      width: "500px",
+      title: "Dejanos tus datos y te enviaremos tu Pineapple Card",
+      html: `<input type="text" id="nombre" class="swal2-input" placeholder="Nombre completo">
+      <input type="text" id="dni" class="swal2-input" placeholder="DNI sin puntos ni espacios">
+<input type="text" id="telefono" class="swal2-input" placeholder="+54 11 0000 0000">
+<input type="text" id="mail" class="swal2-input" placeholder="tuemail@ejemplo.com">
+<input type="text" id="direccion" class="swal2-input" placeholder="Direccion">`,
+confirmButtonText: 'Enviar',
+  })
 }
   return (
     <button style={{
