f79a913be356e4a9be7082ebb645c9647686381b
stefania gentile <stefania.gentile89@icloud.com>
2023-02-07 21:27:59 -0300

    deploy vercel

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const jsonServer = require('json-server')
 const clone = require('clone')
-const data = require('./db.json')
+const data = require('./productos.json')
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'productos.json', {
     _isFake: isProductionEnv
 })
 const middlewares = jsonServer.defaults()
@@ -20,7 +20,7 @@ server.use((req, res, next) => {
 })
 
 server.use(router)
-server.listen(process.env.PORT || 8000, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log('JSON Server is running')
 })
 
