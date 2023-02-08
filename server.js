9b74d21be2ce496df833d32d1c9af4338c46e804
AugustoFons <augustofonsdev@gmail.com>
2023-02-08 20:01:15 -0300

    cambio de puerto

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(5000, () => {
     console.log('JSON Server is running')
 })
 
